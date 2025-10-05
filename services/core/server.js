0c8fccda47c6989e7f04b05785b478c6115c7a0a
SidoIndeedo <sidoindeedo@gmail.com>
2025-10-05 18:09:58 +0530

    fixed integration

--- services/core/server.js
+++ services/core/server.js
@@ -5,13 +5,14 @@ require("dotenv").config();
 const emailRouter = require("./Routes/fetchEmail");
 const cookieParser = require("cookie-parser");
 
+
 const app = express();
 app.use(express.json());  
 app.use(cookieParser());
 
 const port = process.env.PORT || 5001;
 
-// === Connect to MongoDB ===
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected successfully"))
   .catch((err) => console.error("DB connection error:", err));
@@ -21,7 +22,6 @@ app.use(cors({
   origin: 'http://localhost:3000', // frontend URL
   credentials: true,               // allow cookies/auth headers
 }));
-         // parse JSON bodies
 
 // === Routes ===
 app.use("/v1", emailRouter);
@@ -29,4 +29,7 @@ app.use("/v1", emailRouter);
 // === Start Server ===
 app.listen(port, () => {
   console.log(`Core service running on port ${port}`);
+  
+  // Start email watcher
+
 });
